1fcee6e79e1ad20c11b7fe93f9124201d64445ee
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-10-01 19:29:11 +0900

    draw hand on random

--- Drill05/move_character_with_mouse.py
+++ Drill05/move_character_with_mouse.py
@@ -1,10 +1,12 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 
 def handle_events():
@@ -20,17 +22,26 @@ def handle_events():
             running = False
     pass
 
+def character_move():
+    global character_x, character_y
 
+    x1, y1 = character_x, character_y
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 
+character_x = TUK_WIDTH // 2
+character_y = TUK_HEIGHT // 2
+
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    x = random.randint(0, TUK_WIDTH)
+    y = random.randint(0, TUK_HEIGHT)
+    hand.draw(x, y)
     update_canvas()
     frame = (frame + 1) % 8
 
