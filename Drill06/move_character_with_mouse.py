4bc4167d42bc59d524d7df1b4f10e02d0a230205
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-10-01 21:45:35 +0900

    add hand when mouse_left_click

--- Drill06/move_character_with_mouse.py
+++ Drill06/move_character_with_mouse.py
@@ -5,17 +5,21 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 
 def handle_events():
     global running
     global x, y
+    global hand_list
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.button == SDL_BUTTON_LEFT and event.type == SDL_MOUSEBUTTONDOWN:
+            hand_list.append((x, y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -27,7 +31,10 @@ def draw_scene():
 
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    hand.draw(x, y)
+    for (temp_x, temp_y) in hand_list:
+        hand.draw(temp_x, temp_y)
     update_canvas()
     frame = (frame + 1) % 8
 
@@ -36,14 +43,12 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 
+character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
 hand_list = []
 
 while running:
-    clear_canvas()
-    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
+    draw_scene()
 
     handle_events()
 
