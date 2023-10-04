85c1fb41554ad73e23bf350e51e9f8877d385c43
ohtak6843_Laptop <ohtak6843@gmail.com>
2023-10-04 21:30:54 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t값이 1을 넘으면 다시, 새로운 손 화살표가 랜덤 위치에 만들어짐.

--- Drill0506_again/Drill05_solution.py
+++ Drill0506_again/Drill05_solution.py
@@ -36,10 +36,16 @@ def reset_world():
     frame = 0
     action = 3
 
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
     sx, sy = cx, cy  # p1 : 시작점
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
     t = 0.0
+
 
 def render_world():
     clear_canvas()
@@ -62,6 +68,9 @@ def update_world():
         cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy
         t += 0.001
+    else:
+        cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴.
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
