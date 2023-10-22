0e187066d7f0f03fff43371a0705cdbcfe912c5f
ohtak6843_Desktop <ohtak6843@gmail.com>
2023-10-22 20:50:26 +0900

    당구대 그려보기

--- TermProject/main.py
+++ TermProject/main.py
@@ -1,17 +1,20 @@
 from pico2d import *
 
-
-open_canvas(1280, 1024)
+WINDOW_WIDTH, WINDOW_HEIGHT = 1600, 900
+open_canvas(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 
 balls = load_image('Balls.png')
 stick = load_image('Stick.png')
 heart = load_image('Heart.png')
+table = load_image('Table.jpg')
 
 
 while (True):
+    table.draw(800, 450, 1600, 1000)
     balls.draw(400, 0, 150, 150)  # 1/10 사이즈
     stick.draw(500, 500, 240, 240)  # 1/5 사이즈
     heart.draw(800, 200, 66, 60)  # 1/10 사이즈
     update_canvas()
+    # handle_events()
     delay(0.1)
